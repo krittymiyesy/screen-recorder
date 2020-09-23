dfb2c64e138e039460e6b6839929acbafa5de497
WuChao <krittymiyesy@outlook.com>
2020-09-23 18:36:18 +0800

    1
    1

--- Test/mian.cpp
+++ Test/mian.cpp
@@ -1,4 +1,3 @@
-
 #define AMRECORDER_IMPORT
 #include "../Recorder/export.h"
 
@@ -20,12 +19,12 @@ int main()
 	AMRECORDER_DEVICE *speakers = NULL, *mics = NULL;
 	AMRECORDER_ENCODERS *vencoders = NULL;
 
-	AMRECORDER_SETTING setting = {0};
-	AMRECORDER_CALLBACK callback = {0};
+	AMRECORDER_SETTING setting = { 0 };
+	AMRECORDER_CALLBACK callback = { 0 };
 
 
 	int nspeaker = recorder_get_speakers(&speakers);
-	
+
 	int nmic = recorder_get_mics(&mics);
 
 	int n_vencoders = recorder_get_vencoders(&vencoders);
@@ -33,10 +32,28 @@ int main()
 
 	setting.v_left = 0;
 	setting.v_top = 0;
-	setting.v_width = GetSystemMetrics(SM_CXSCREEN);
-	setting.v_height = GetSystemMetrics(SM_CYSCREEN);
+	//setting.v_width = GetSystemMetrics(SM_CXSCREEN);
+	//setting.v_height = GetSystemMetrics(SM_CYSCREEN);
+	HDC hdc = GetDC(NULL);
+	setting.v_width = GetDeviceCaps(hdc, DESKTOPHORZRES);
+	setting.v_height = GetDeviceCaps(hdc, DESKTOPVERTRES);
+	ReleaseDC(NULL, hdc);
+
+	//setting.v_left = 100;
+	//setting.v_top = 100;
+	//setting.v_width = 1920;
+	//setting.v_height = 1080;
+
+	if (setting.v_width % 32 != 0) {
+		setting.v_width = setting.v_width / 32 * 32;
+	}
+	if (setting.v_height % 2 != 0) {
+		setting.v_height = setting.v_height / 2 * 2;
+	}
+	
 	setting.v_qb = 100;
 	setting.v_bit_rate = 1280 * 1000;
+	//setting.v_bit_rate = 1280 * 720;
 	setting.v_frame_rate = 30;
 
 	//////////////should be the truely encoder id,zero will always be soft x264
@@ -52,7 +69,7 @@ int main()
 	}
 #endif
 
-#if 1 //record mic
+#if 1 //record microphone
 	for (int i = 0; i < nmic; i++) {
 		if (mics[i].is_default == 1)
 			memcpy(&setting.a_mic, &mics[i], sizeof(AMRECORDER_DEVICE));
@@ -69,17 +86,12 @@ int main()
 
 	//CAN NOT PAUSE FOR NOW
 	/*getchar();
-
 	recorder_pause();
-
 	printf("recorder paused\r\n");
-
 	getchar();
-
 	recorder_resume();
-
 	printf("recorder resumed\r\n");*/
-
+	//input enter key,then recording stopped
 	getchar();
 
 
